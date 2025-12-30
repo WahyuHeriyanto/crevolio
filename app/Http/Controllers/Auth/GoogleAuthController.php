ae08d7b5c4622764b488d2528f587390341e00c3
WahyuHeriyantoThrive <wahyu.heriyanto@thrive.co.id>
2025-12-31 00:24:58 +0700

    Fixing bug google auth

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -22,10 +22,34 @@ class GoogleAuthController extends Controller
         $user = User::where('email', $googleUser->getEmail())->first();
 
         if (! $user) {
+
+            // ===== Generate username =====
+            $baseUsername = Str::slug($googleUser->getName());
+            $username = $baseUsername;
+            $i = 1;
+
+            while (User::where('username', $username)->exists()) {
+                $username = "{$baseUsername}{$i}";
+                $i++;
+            }
+
+            // ===== Generate slug =====
+            $baseSlug = $username;
+            $slug = $baseSlug;
+            $i = 1;
+
+            while (User::where('slug', $slug)->exists()) {
+                $slug = "{$baseSlug}-{$i}";
+                $i++;
+            }
+
             $user = User::create([
                 'name' => $googleUser->getName(),
+                'username' => $username,
+                'slug' => $slug,
                 'email' => $googleUser->getEmail(),
                 'password' => bcrypt(Str::random(16)),
+                'access_level' => 0, 
             ]);
         }
 
